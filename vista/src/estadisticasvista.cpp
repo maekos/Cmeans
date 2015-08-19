2af99a785442dbc1c11030449222dcfa20f5d3af
Marcos Alfonzo <malfonzo@sti-tech.com.ar>
2015-08-19 00:06:57 -0300

    [MZA] +Calculo de Matriz de correlacion.
    Finalizada la vista de estadisticas.

--- vista/src/estadisticasvista.cpp
+++ vista/src/estadisticasvista.cpp
@@ -99,7 +99,8 @@ void EstadisticasVista::on_nextButton_clicked()
 void EstadisticasVista::on_estadisticasBoton_clicked()
 {
     this->ui->plainTextEdit->clear();
-    Stats *sta[100];
+    Stats *sta[100]; // Cuidado de no elegir mas de 100 variables.
+
     this->ui->plainTextEdit->insertPlainText("Estadisticas\n");
     this->ui->plainTextEdit->insertPlainText("Numero de Variables: " +
                                              QString::number(matriz.size1()));
@@ -107,7 +108,8 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                                              QString::number(matriz.size2()) +
                                              "\n\n");
 
-
+/******************************************************************************/
+    // Calculo de estadisticas
     for(int i = 0; i< matriz.size1();i++)
     {
         Vector v;
@@ -115,22 +117,27 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
         sta[i]  = new Stats(&v);
     }
 
-    // Matriz de covarianza
-    Matrix covar;
+    // Matriz de covarianza y coeficiente de correlacion
+    Matrix covar, cc;
     covar.resize(matriz.size1(),matriz.size1());
+    cc.resize(matriz.size1(),matriz.size1());
     Vector v1, v2;
-    for(int i = 0; i< matriz.size1(); i++){
+    for(int i = 0; i< matriz.size1(); i++){        
         v1 = boost::numeric::ublas::row(matriz,i);
+        float stdV1 = Stats::desviacionEstandar(&v1);
         for(int j = i; j < matriz.size1(); j++)
         {
             v2 = boost::numeric::ublas::row(matriz,j);
+            float stdV2 = Stats::desviacionEstandar(&v2);
             float cv = Stats::Covarianza(&v1,&v2);
+            cc.insert_element(i,j,cv/(stdV1*stdV2));
+            cc.insert_element(j,i,cv/(stdV1*stdV2));
             covar.insert_element(i,j,cv);
             covar.insert_element(j,i,cv);
         }
     }
 
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("Mean\n");
     foreach(const QString col, this->columns)
     {
@@ -143,7 +150,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                 insertPlainText(
                     QString::number(sta[i]->getMedia()) + "\t\t");
     }
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nStandar Deviation\n");
     foreach(const QString col, this->columns)
     {
@@ -156,7 +163,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                 insertPlainText(
                     QString::number(sta[i]->getDesviacionEstandar()) + "\t\t");
     }
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nCoefficient of variation (%)\n");
     foreach(const QString col, this->columns)
     {
@@ -171,8 +178,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                         sta[i]->getDesviacionEstandar() / sta[i]->getMedia() *
                         100) + "\t\t");
     }
-
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nMinimum values\n");
     foreach(const QString col, this->columns)
     {
@@ -185,7 +191,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                 insertPlainText(
                     QString::number(sta[i]->getMinNumber()) + "\t\t");
     }
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nMaximum values\n");
     foreach(const QString col, this->columns)
     {
@@ -198,7 +204,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                 insertPlainText(
                     QString::number(sta[i]->getMaxNumber()) + "\t\t");
     }
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nSum of squares\n");
     foreach(const QString col, this->columns)
     {
@@ -211,7 +217,7 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
                 insertPlainText(
                     QString::number(sta[i]->getSumaCuadrados()) + "\t\t");
     }
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\n\nVariance-Covariance Matrix\n");
     foreach(const QString col, this->columns)
     {
@@ -229,13 +235,22 @@ void EstadisticasVista::on_estadisticasBoton_clicked()
         }
         this->ui->plainTextEdit->insertPlainText("\n");
     }
-
-
-
+/******************************************************************************/
     this->ui->plainTextEdit->insertPlainText("\nCorrelation Matrix\n");
     foreach(const QString col, this->columns)
     {
-        this->ui->plainTextEdit->insertPlainText("\t" + col + "\t\t");
+        this->ui->plainTextEdit->insertPlainText("\t" + col + "\t");
     }
     this->ui->plainTextEdit->insertPlainText("\n");
+    for(int i = 0; i < matriz.size1(); i++){
+        this->ui->plainTextEdit->insertPlainText(this->columns.at(i) + "\t");
+        for(int j = 0; j < matriz.size1(); j++)
+        {
+            this->ui->plainTextEdit->insertPlainText(
+                                    QString::number(cc.at_element(i,j),'f',3)
+                                                     + "\t\t");
+        }
+        this->ui->plainTextEdit->insertPlainText("\n");
+    }
+/******************************************************************************/
 }
